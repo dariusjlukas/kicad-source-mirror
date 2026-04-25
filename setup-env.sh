09a6ea4c72dc893ce8c990b443d6f9142f750779
Darius J. Lukas <dariusjlukas@gmail.com>
2026-04-25 11:13:47 -0400

    Update setup-env.sh for Fedora

--- setup-env.sh
+++ setup-env.sh
@@ -2,7 +2,7 @@
 # Source this file to set up the environment for running KiCad from ~/bin/kicad
 # Usage: source setup-env.sh
 
-export LD_LIBRARY_PATH="$HOME/bin/kicad/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$HOME/bin/kicad/lib:$HOME/bin/kicad/lib64:$LD_LIBRARY_PATH"
 export PATH="$HOME/bin/kicad/bin:$PATH"
 
 echo "KiCad environment configured:"
